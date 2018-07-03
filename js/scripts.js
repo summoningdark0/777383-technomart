38783597ab9c18e298ded1e59cd49e3805e30c72
Dmitriy Galkin <r-i-d@yandex.ru>
2018-07-03 21:02:50 +0300

    External js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,45 @@
-var servTab = document.querySelectorAll(".service-option");
-//var servContent = document.querySelectorAll(".service-description");
+	var purchasePopup = document.querySelector(".purchase-popup");
+		var purchaseLink = document.querySelectorAll(".button-add-to-cart");
+		var purchaseClose = document.querySelector(".purchase-close");
+		var feedbackPopup = document.querySelector(".feedback-popup");
+		var feedbackLink = document.querySelector(".button-contact-us");
+		var feedbackClose = document.querySelector(".feedback-close");
+		var mapPopup = document.querySelector(".map-popup");
+		var mapLink = document.querySelector(".map");
+		var	mapClose = document.querySelector(".map-close");
 
+		// Попап заказа
+		for (var i = 0; i < purchaseLink.length; i++){
+			purchaseLink[i].addEventListener("click", function(evt) {
+			evt.preventDefault();
+			// console.log("Now I Became Horse");
+			purchasePopup.classList.remove("display-none");
+		});
+		}
 
-//window.onload(tabClick());
-function horse(a) {
-  "use strict";
-  if (servTab[a].classList.contains("service-option-link")) {
-    console.log("horse");
-  }
-}
+		purchaseClose.addEventListener("click", function(evt) {
+			evt.preventDefault();
+			purchasePopup.classList.add("display-none");
+		});
 
-function tabClick() {
-  "use strict";
-  var i=0;
-  for ( i=0; i < servTab.length; i +=1) {
-    servTab[i].addEventListener("click", horse(i));
-}}
+		// попап отзыва
+
+		feedbackLink.addEventListener("click", function(evt) {
+			evt.preventDefault();
+			feedbackPopup.classList.remove("display-none");
+		});
+
+		feedbackClose.addEventListener("click", function(evt){
+			evt.preventDefault();
+			feedbackPopup.classList.add("display-none");
+		});
+		// попап карты
+		mapLink.addEventListener("click", function(evt) {
+			evt.preventDefault();
+			mapPopup.classList.remove("display-none");
+		});
+
+		mapClose.addEventListener("click", function(evt)	{
+			evt.preventDefault();
+			mapPopup.classList.add("display-none");
+		});
